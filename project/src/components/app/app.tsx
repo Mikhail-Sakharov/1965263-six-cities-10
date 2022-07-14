ae7d37483b069bc23703649c86b381bda3ba3c58
Mikhail-Sakharov <sakharov_mikhail@hotmail.com>
2022-07-14 15:12:32 +0300

    Реализовал маршрутизацию, вынес логотип в отдельный компонент, добавил страницу 404

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,47 @@
-import Main from '../../pages/main';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Room from '../../pages/room/room';
+import NotFoundPage from '../../pages/not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
 
 type MainComponentProps = {
   rentOffersCount: number
 };
 
 function App({rentOffersCount}: MainComponentProps): JSX.Element {
-  return <Main rentOffersCount={rentOffersCount}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main rentOffersCount={rentOffersCount}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <Favorites/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<Room/>}
+        />
+        <Route
+          path={AppRoute.NotFound}
+          element={<NotFoundPage/>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/pages/not-found/not-found.tsx
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom';
+import Logo from '../../components/logo/logo';
+
+function NotFoundPage(): JSX.Element {
+  return (
+    <div className="not-found__status-wrapper">
+      <Logo/>
+      <h1>
+            404.
+        <br />
+        <small>Page not found</small>
+      </h1>
+      <Link to="/">Go to main page</Link>
+    </div>
+  );
+}
+
+export default NotFoundPage;
