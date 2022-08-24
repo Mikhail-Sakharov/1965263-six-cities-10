fbd95842191000966767cff392a1d7c3d2a1bfd1
Mikhail-Sakharov <sakharov_mikhail@hotmail.com>
2022-08-24 20:06:05 +0300

    Рефакторинг

--- project/src/components/main-empty/main-empty.test.tsx
+++ project/src/components/main-empty/main-empty.test.tsx
@@ -1,16 +1,27 @@
+import {configureMockStore} from '@jedmao/redux-mock-store';
 import {render, screen} from '@testing-library/react';
 import {createMemoryHistory} from 'history';
+import { Provider } from 'react-redux';
 import HistoryRouter from '../../components/history-route/history-route';
 import MainEmpty from './main-empty';
+
+const mockStore = configureMockStore();
+const store = mockStore({
+  DATA: {
+    city: 'Paris'
+  }
+});
 
 describe('Component: MainEmpty', () => {
   it('should render correctly', () => {
     const history = createMemoryHistory();
 
     render(
-      <HistoryRouter history={history}>
-        <MainEmpty/>
-      </HistoryRouter>
+      <Provider store={store}>
+        <HistoryRouter history={history}>
+          <MainEmpty/>
+        </HistoryRouter>
+      </Provider>
     );
 
     const statusElement = screen.getByText(/No places to stay available/i);
