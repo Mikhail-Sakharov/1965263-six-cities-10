95934b9144215dc922495fc447d3647afac3a855
Mikhail-Sakharov <sakharov_mikhail@hotmail.com>
2022-08-29 21:07:10 +0300

    Добавил тесты для страниц

--- project/src/pages/loading-screen/loading-screen.tsx
+++ project/src/pages/loading-screen/loading-screen.tsx
@@ -3,7 +3,7 @@ import styles from './loading-screen.module.css';
 
 function LoadingScreen(): JSX.Element {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} data-testid="loadingScreen">
       <div className={styles.spinner}>
         <RingLoader color={'#4481C3'} size={70}/>
       </div>
